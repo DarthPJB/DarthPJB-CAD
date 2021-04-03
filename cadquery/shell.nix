4003732b6ad8fb2d16a1ee85818ec9fbac2f137f
john bargman <darthpjb@gmail.com>
2021-04-03 01:36:19 +0100

    updated nix-shell

--- cadquery/shell.nix
+++ cadquery/shell.nix
@@ -1,9 +1,45 @@
+# Generate a nix-shell for compiling cadquery, editing source
+# and viewing results
+
+#default nixpkgs
 { pkgs ? import <nixpkgs> {} }:
+
+let
+baseconfig = { allowUnfree = true; };
+unstableTarball =
+  fetchTarball
+    https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+unstable = import unstableTarball
+{
+  config = baseconfig;
+};
+in
+
+# Generate Shell
 pkgs.mkShell
 {
-  buildInputs = [ pkgs.python37 pkgs.python37Packages.cadquery pkgs.fstl];
+  buildInputs = [
+  #Python37 and cadquery python37Packages; this provides basic ability
+  # to use python CadQueryModel.py to generate output.
+  pkgs.python37
+  pkgs.python37Packages.cadquery
+  #CQ-editor for native rendering of CadQuery Models
+  unstable.cq-editor
+  # FastSTL viewer to view resulting STL files
+  pkgs.fstl
+  # Inkscape for the inkview package (fast SVG viewer)
+  pkgs.inkscape
+  # atom and vim for effective code editing
+  pkgs.atom pkgs.vim
+  pkgs.figlet
+  ];
+  #Run build-task post generation (TODO: makefile)
   shellHook = ''
-     ./task.sh;
-     exit;
+      figlet "Shell Active:"
+      echo "starting editors"
+      atom ./
+      cq-editor ./88/test.py&
+      echo "to begin build sequence; run -"
+      echo "./task.sh;"
   '';
 }
