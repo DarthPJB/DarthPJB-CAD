1c93b17d3ca7c8593c60d006ea3e7f8daa9bb2c9
John Bargman <darthpjb@gmail.com>
2022-04-14 15:23:31 +0100

    preparing a nix shell for colmap automation

---
+++ colmap/shell.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let fhs = pkgs.buildFHSUserEnv {
+        name = "cuda-env";
+        targetPkgs = pkgs: with pkgs;
+               [ git
+                 gitRepo
+                 gnupg
+                 autoconf
+                 curl
+                 procps
+                 gnumake
+                 utillinux
+                 m4
+                 gperf
+                 unzip
+                 colmapWithCuda
+                 cudatoolkit
+                 linuxPackages.nvidia_x11
+                 libGLU
+                 xorg.libXi xorg.libXmu freeglut
+                 xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
+                 ncurses5
+                 stdenv.cc
+                 binutils
+                 bash
+                ];
+          multiPkgs = pkgs: with pkgs; [ zlib ];
+          runScript = "bash";
+          profile = ''
+                  export CUDA_PATH=${pkgs.cudatoolkit}
+                  # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
+                  export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+                  export EXTRA_CCFLAGS="-I/usr/include"
+            '';
+          };
+in pkgs.stdenv.mkDerivation {
+   name = "cuda-env-shell";
+   nativeBuildInputs = [ fhs ];
+   shellHook = "exec cuda-env";
+}
+
